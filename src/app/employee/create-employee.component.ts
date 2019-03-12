c01853ea516b31070ccbb6285ef15b1948c42b89
Mahmoud <melkhatib@amaris.com>
2019-03-12 17:24:00 +0100

    created department model

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
+import { Employee } from 'src/app/models/employee.model';
 @Component({
   selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
@@ -7,11 +8,23 @@ import {NgForm} from '@angular/forms';
 })
 export class CreateEmployeeComponent implements OnInit {
 
-  gender="mal"
-  constructor() { 
-
-   
-  }
+  employee: Employee = {
+    id: null,
+    name: null,
+    gender: null,
+    contactPreference: null,
+    phoneNumber: null,
+    email: '',
+    dateOfBirth: null,
+    department: '-1',
+    isActive: null,
+    photoPath: null
+  };
+  departments : Department[]= [
+{id : 1 , name:"Help Desk"},
+{id : 2 , name:"Web"},
+{id : 3 , name:"CRM"}
+];
   saveEmployee(employeeForm: NgForm): void {
     console.log(employeeForm.value);
   }
---
+++ src/app/models/Department.model.ts
@@ -0,0 +1,4 @@
+class Department {
+    id : number;
+    name : string; 
+}
